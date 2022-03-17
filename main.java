14b17733c03c626a702ffec94c6bb795b2c423ef
hoang viet long <hoang.viet.long.sys21@morijyobi.ac.jp>
2022-03-17 12:13:11 +0900

    update main.java

--- main.java
+++ main.java
@@ -1,9 +1,14 @@
+import java.util.*;
 public class main {
 	public static void main(String[] args){
+		Scanner stdin = new Scanner(System.in);
 		double height ,weight , bmi;
-		height = 73;
-		weight = 1.73;
-		bmi = height / (weight*weight);
-		System.out.println( "BMI:" + String.format("%.1f", bmi) );
+		int i;
+		for( i = 0 ; i < 2 ; i++ ){	
+                	height = stdin.nextDouble();
+			weight = stdin.nextDouble();
+			bmi = height / (weight*weight);
+			System.out.println( "BMI:" + String.format("%.1f", bmi) );
+		}
 	}
 }
